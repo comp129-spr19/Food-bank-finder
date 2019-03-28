a42fc7bfba786fcb1ae38b0d5aefbf97b99ae39d
Mark Fraser <m_fraser3@u.pacific.edu>
2019-03-28 15:13:07 -0700

    Implement name-tests-test Pre-Commit Hook

---
+++ tests/unit_tests/travis_ci_builds_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import unittest
+
+
+class TestTravisCI(unittest.TestCase):
+
+    """
+    dummy test to check that Travis CI works
+    """
+    def test_true(self):
+        self.assertTrue(True)
