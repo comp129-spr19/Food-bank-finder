d6cf48c41470f82616f0b0afd00c68b9927e35b2
Dharak Vasavda <vasavdadharak@gmail.com>
2019-03-28 18:29:04 -0700

    Linked button to Google Maps page
    As the customer requested, we were able to link the "Find Foodbanks" button to another app (webapp) which will display the Google Maps embed in the future.
    @neunis @brforest @dvasavda

--- donatefood/map_page/urls.py
+++ donatefood/map_page/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^foodbank_results', views.get_foodbank_results)
 ]
--- donatefood/map_page/views.py
+++ donatefood/map_page/views.py
@@ -1,16 +1,6 @@
-import json
-import urllib.request
-
 from django.shortcuts import render
 
-
-def get_foodbank_results(request):
-    with urllib.request.urlopen(
-            "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-            "foodbank+in+stockton&key=AIzaSyCJltxwttdE1THuT4ogdGHtsM6VWJ_T1mU"
-            ) as url:
-        data = json.loads(url.read().decode())
-    return data
+# Create your views here.
 
 
 def index(request):
