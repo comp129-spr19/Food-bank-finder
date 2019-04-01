37d89ef3b6e45b40db60036e945ef2723f8b3e5f
Mark Fraser <m_fraser3@u.pacific.edu>
2019-04-01 16:00:37 -0700

    Change Repo to Create Runnable setup.py (#45)

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from pip.req import parse_requirements
 from setuptools import setup
 
-setup(
-   name='donatefood',
-   version='1.0',
-   description='A webpage dedicated to providing information about food '
-   + 'donation centers',
-   author='Mark Fraser',
-   author_email='m_fraser3@u.pacific.edu',
-   packages=['donatefood'],
-   install_requires=parse_requirements('requirements.txt', session='hack'),
-)
+REQUIRED_PACKAGES = [
+    'django==2.1.7',
+    'pycodestyle==2.5.0',
+    'pre-commit==1.14.4',
+    'setuptools==27.2.0'
+]
+
+if __name__ == "__main__":
+    setup(
+       name='donatefood',
+       version='1.0',
+       description='A webpage dedicated to providing information about food '
+       + 'donation centers',
+       author='Mark Fraser',
+       author_email='m_fraser3@u.pacific.edu',
+       packages=['donatefood'],
+       install_requires=REQUIRED_PACKAGES,
+    )
