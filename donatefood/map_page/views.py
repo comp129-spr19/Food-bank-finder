4958dac7b952d670236103254dea57ae5ee5e132
Jillian <j_david1@u.pacific.edu>
2019-03-31 23:22:55 -0700

    Resolving merge conflicts and adding Google Maps embed

--- donatefood/map_page/views.py
+++ donatefood/map_page/views.py
@@ -1,6 +1,16 @@
+import json
+import urllib.request
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_foodbank_results(request):
+    with urllib.request.urlopen(
+            "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
+            "foodbank+in+stockton&key=AIzaSyCJltxwttdE1THuT4ogdGHtsM6VWJ_T1mU"
+            ) as url:
+        data = json.loads(url.read().decode())
+    return data
 
 
 def index(request):
