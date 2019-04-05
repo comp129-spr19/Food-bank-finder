f418d4c6817461e1a52ed936dd3c3dc0a305bfe7
Beau <brforest@gmail.com>
2019-04-05 15:49:22 -0700

    Added events and needs template
    @brforest @neunis @dvasavda

--- donatefood/events_needs/views.py
+++ donatefood/events_needs/views.py
@@ -1,2 +1,7 @@
-# from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    # return HttpResponse("<h1>Testing donatefood/home/views.py</h1>")
+    return render(request, 'events_needs/events_needs.html')
