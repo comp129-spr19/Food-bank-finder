b035c7bb87bb9ada509978e96d169c3dc0be0203
Mark Fraser <m_fraser3@u.pacific.edu>
2019-04-06 17:23:06 -0700

    Add QUnit Testing Infrastructure to the repository

---
+++ tests/unit_tests/js/README.md
@@ -0,0 +1,4 @@
+# Javascript QUnit Test Directory
+
+Tests in this directory shall follow the format `<name>_test.js` to be included
+in the test suite for Javascript Files
---
+++ tests/unit_tests/js/qunit_functionality_test.js
@@ -0,0 +1,5 @@
+// This test file asserts that the QUnit Javascript test suite is run
+
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
