2069da5404228c27aabb8456df2573cf6b06c280
Mark Fraser <m_fraser3@u.pacific.edu>
2019-04-12 10:40:37 -0700

    Attempt to resolve installation dependencies in Travis

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 REQUIRED_PACKAGES = [
-    'django==2.1.7',
-    'pre-commit==1.14.4',
-    'pycodestyle==2.5.0',
-    'selenium==3.141.0',
-    'setuptools==27.2.0'
+  'django==2.1.7',
+  'pre-commit==1.14.4',
+  'pycodestyle==2.5.0',
+  'selenium==3.141.0',
+  'setuptools==27.2.0',
+  'zipp>=0.3.2'
 ]
 
 if __name__ == "__main__":
