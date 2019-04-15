190a066d4062d22f34115bf762407dc8ffd814de
Neunis <n_nunis@u.pacific.edu>
2019-04-15 16:46:04 -0700

    added path for settings.py to recognize table and models foreign key currently commented out

--- donatefood/events_needs/models.py
+++ donatefood/events_needs/models.py
@@ -9,11 +9,12 @@ from django.conf import settings
 # Within the table created are the fields of our table
 # Todo: eradicate magic numbers
 class FoodBanks(models.Model):
-    #food_bank_id = models.ForeignKey("apps.Model", on_delete=models.CASCADE)
+    food_bank_id = models.ForeignKey("apps.Model", on_delete=models.CASCADE)
     food_bank_name = models.CharField(max_length = 100)
 
 
 class FoodBankEvents(models.Model):
+    #food_bank_event_id = models.ForeignKey()
     food_bank_event = models.CharField(max_length = 100)
     food_bank_description = models.CharField(max_length = 500)
     food_bank_date = models.DateTimeField('Event Date')
