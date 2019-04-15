b0bf8e433514a0f428df63e9531d1464e35f82e3
Neunis <n_nunis@u.pacific.edu>
2019-04-14 21:20:41 -0700

    Database Table Created

--- donatefood/events_needs/models.py
+++ donatefood/events_needs/models.py
@@ -1,2 +1,11 @@
-# from django.db import models
-# Create your models here.
+#this is import is needed/necessary for any table to be created
+from django.db import models 
+
+# Table is created here.
+# The name of the clss is the name of the respective table
+# Since this is an instance of Model we use model.Model
+# Within the table created are the fields of our table
+class FoodBanks(models.Model):
+    food_bank_name = models.CharField(max_length = 100)
+    food_bank_event = models.CharField(max_length = 100)
+    food_bank_description = models.CharField(max_length= 500)
