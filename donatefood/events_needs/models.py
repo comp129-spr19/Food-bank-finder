00a3d5a903a47d5f97e1a5e0edbe4521d89c6962
Neunis <n_nunis@u.pacific.edu>
2019-04-14 22:14:07 -0700

    Refined tables/models for holding foodbank info and events respectively

--- donatefood/events_needs/models.py
+++ donatefood/events_needs/models.py
@@ -1,11 +1,19 @@
 #this is import is needed/necessary for any table to be created
-from django.db import models 
+from django.db import models
+from django.conf import settings  
+#django.utils.timezone
 
 # Table is created here.
 # The name of the clss is the name of the respective table
 # Since this is an instance of Model we use model.Model
 # Within the table created are the fields of our table
+# Todo: eradicate magic numbers
 class FoodBanks(models.Model):
+    #food_bank_id = models.ForeignKey("apps.Model", on_delete=models.CASCADE)
     food_bank_name = models.CharField(max_length = 100)
+
+
+class FoodBankEvents(models.Model):
     food_bank_event = models.CharField(max_length = 100)
-    food_bank_description = models.CharField(max_length= 500)
+    food_bank_description = models.CharField(max_length = 500)
+    food_bank_date = models.DateTimeField('Event Date')
