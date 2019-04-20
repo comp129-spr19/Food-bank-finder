5c4a1528c326d411c7fb4e64a6d53584753b4bf7
Neunis <n_nunis@u.pacific.edu>
2019-04-20 15:17:42 -0700

    Experimenting schema (#76)
    * simple table framework
    
    * Change Google Maps JS Loading to API Code (needs API 3 syntax)
    
    * Add button linking to 'events_needs/' on home page
    
    * Delete __init__.cpython-37.pyc
    
    * Delete admin.cpython-37.pyc
    
    * Delete models.cpython-37.pyc
    
    * Delete urls.cpython-37.pyc
    
    * Delete views.cpython-37.pyc
    
    * Delete __init__.cpython-37.pyc
    
    * Add working embedded Google Maps map and search results
    
    * Fix search to return filtered results
    
    This commit adds keyword filtering to the nearby search.  Now the
    results are filtered by "food bank" keyword.
    
    * Results display in table
    
    The query results now display in the table below the map.  Currently,
    only the name and address of the item display.
    
    * Added place details
    
    This commit adds place details in the table below the map.
    
    Note: this does not show the
    details on the map with the icons; therefore this commit incurs a little technical debt, as it will take a little repositioning of code to efficiently use the detailed information both in the table and on the map icons.
    
    * Add click listeners to map icons
    
    Map icons now show information about the item when clicked on.
    
    * Remove unnecessary __pycache__/ files and update .gitignore files
    
    * Database Table Created
    
    * Refined tables/models for holding foodbank info and events respectively
    
    * added path for settings.py to recognize table and models foreign key currently commented out
    
    * Pages updated across all apps
    
    * Database key already generated at creation of table in Django, deleted obsolete var
    
    * Migrations altering tables created for foodbank and foodbank events respectively
    
    * Working out test of DB
    
    * Finalized General Creation of Database
    
    -Refactored foreign key connecting food bank events to food bank model
    
    -Method for information stored in models to printed out created
    
    -test out and added some cases to show storage within tables
    
    * Admin access in terminal and localhost
    
    -  tables can now be accessed on the localhost/admin to make changes
    
    * Time Zone reset to default settings in settings.py
    
    * Models.py Foreign Key Column temp removed
    
    * added test to mini db storage

--- donatefood/events_needs/admin.py
+++ donatefood/events_needs/admin.py
@@ -1,2 +1,8 @@
-# from django.contrib import admin
-# Register your models here.
+from django.contrib import admin
+# this imports the models/tables we created 
+# in order to show on the admin site
+from .models import FoodBanks, FoodBankEvents
+
+# this line allows us to see the models 
+# on the localhost admin site
+admin.site.register(FoodBanks)
