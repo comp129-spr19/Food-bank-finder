1ae6939c394707217694cd128c1282ea9f925caf
Jillian <j_david1@u.pacific.edu>
2019-04-22 17:30:49 -0700

    view edit

--- donatefood/events_needs/views.py
+++ donatefood/events_needs/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import (
+		TemplateView, 
+		ListView
+)
+from .models import FoodBanks
+
 
 
 def index(request):
     # return HttpResponse("<h1>Testing donatefood/home/views.py</h1>")
     return render(request, 'events_needs/events_needs.html')
+
+class EventView(TemplateView):
+	template_name = 'events_needs/events_needs.html'
+
+	def get(self, request):
+		return Foodbanks.objects.all()
