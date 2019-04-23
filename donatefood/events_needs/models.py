553483d7a930590df59866e6ed2dd1809224c7b1
Neunis <n_nunis@u.pacific.edu>
2019-04-23 13:26:01 -0700

    Events Form Creation
    -Models.py refactored to allow all data in table to be displayed when called on
    -views.py reset to original setup until further db incorporations are made
    -events_needs.html has front-end form created for user to add to database

--- donatefood/events_needs/models.py
+++ donatefood/events_needs/models.py
@@ -7,8 +7,6 @@ from django.conf import settings
 # The name of the clss is the name of the respective table
 # Since this is an instance of Model we use model.Model
 # Within the table created are the fields of our table
-# Todo: eradicate magic numbers
-
 
 class FoodBankEvents(models.Model):
     food_bank_name = models.CharField(max_length=100, default = 'Food Bank Name')
@@ -16,5 +14,6 @@ class FoodBankEvents(models.Model):
     food_bank_date = models.DateTimeField('Event Date')
     food_bank_description = models.TextField(blank=True)
 
-    def get_name(self):
-        return self.food_bank_name
+    def __str__(self):
+        return self.food_bank_name + ' ' + self.food_bank_event + ' ' + \
+            self.food_bank_date + ' ' + self.food_bank_description
