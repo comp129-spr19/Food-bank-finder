4a011bdb5b8ffe7112559d8d29c525af6c14ef08
Neunis <n_nunis@u.pacific.edu>
2019-04-26 19:15:21 -0700

    Post method for form implemented to send data to database

--- donatefood/events_needs/urls.py
+++ donatefood/events_needs/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     #events needs home page
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    #TODO: once db can pull from form and add event separate link is added
+    #then add form for separate form to view
+    #url(r'^add_event/$', views.AddEvent, name= 'add_event'),
+    url(r'^add_event_form_submission/', views.add_event_form_submission, name= 'add_event_form_submission'),
 ]
--- donatefood/events_needs/views.py
+++ donatefood/events_needs/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import FoodBankEvents
 
 def index(request):
 	return render(request, 'events_needs/events_needs.html')
+
+#TODO: Add this as separate page once database can pull data from form and be displayed
+#def AddEvent(request):
+#	return render (request, "AddEvent/add_event.html")
+
+def add_event_form_submission(request):
+	print("Form has been submitted") #printed on terminal
+	fbname = request.POST["fbname"]
+	ename = request.POST["ename"]
+	date = request.POST["date"]
+	descript = request.POST["descript"]
+
+	food_bank_info = FoodBankEvents(food_bank_name = fbname, food_bank_event = ename, 
+									food_bank_date = date, food_bank_description = descript)
+	
+	food_bank_info.save()
+	
+	return render(request, "events_needs/events_needs.html")
 
